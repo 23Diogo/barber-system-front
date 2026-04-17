bd0e956bebc813aa62c7d06b0170c02b684932ac
23Diogo <129696135+23Diogo@users.noreply.github.com>
2026-04-17 10:13:47 -0300

    Delete barberflow/js/client-entry.js

--- barberflow/js/client-entry.js
+++
@@ -1,6 +0,0 @@
-import { initClientRouter } from './client/client-router.js';
-
-window.addEventListener('DOMContentLoaded', () => {
-  console.log('[CLIENT] client-entry carregado');
-  initClientRouter();
-});
