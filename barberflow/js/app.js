47eb91df46828a972307da0249b626a3c350b723
23Diogo <129696135+23Diogo@users.noreply.github.com>
2026-04-18 15:26:24 -0300

    Update app.js

--- barberflow/js/app.js
+++ barberflow/js/app.js
@@ -5,9 +5,13 @@ import { initWidgets } from './components/widgets.js';
 import { initDevAuth } from './components/dev-auth.js';
 import { initDashboard } from './modules/dashboard.js';
 import { navigate, initRouter } from './router.js';
+import { initMobileSidebar } from './components/mobile-sidebar.js';
+
+
 
 function initAdminApp() {
   bindSidebar(navigate);
+  initMobileSidebar(); 
   initThemeToggle();
   initModal();
   initDashboard();
